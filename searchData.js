5cbf6d9e7617dce513316143c0bcdafc66b1f75f
matthewstathis <45523053+matthewstathis@users.noreply.github.com>
2025-11-24 12:44:19 -0500

    Update searchData.js

--- searchData.js
+++ searchData.js
@@ -329,7 +329,7 @@ const searchItems = [
         { name: "StartSmart Job Aid", url: "https://portaldoctool.cvs.com/retailportalfiles/radar_store/temp/fm/StartSmart_Job_Aid_20250922145418800.pdf" },
         { name: "StartSmart Participant Guide", url: "https://portaldoctool.cvs.com/retailportalfiles/radar_store/temp/store/SmartStart_Participant_Guide_20250808144624526.pdf" },
         { name: "Electronic Signature Portal", url: "https://rxdw-esignature-store.rxdw-cld-ap.cvshealthcloud.com/GetSMEsigStore.do?method=perform&StoreNum=XXXXX" },
-
+        { name: "Clinical Pharmacology Remote Access Setup Link", url: "https://www.clinicalkey.com/pharmacology/login?promoCodeGroup=DID416516&promoCode=416516&associatingWith=CVS%20Co,%20General%20Access" },
 
 
         { name: "CVS\Domain Apps Portal (Citrix) ", url: "https://remote.cvshealth.com/Citrix/remotecvsWeb/" }
