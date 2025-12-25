bdc6235bd971c7a048a45f09b49f1dfd2bc905a7
matthewstathis <45523053+matthewstathis@users.noreply.github.com>
2025-12-24 22:42:33 -0500

    Update searchData.js

--- searchData.js
+++ searchData.js
@@ -332,6 +332,7 @@ const searchItems = [
         { name: "Clinical Pharmacology Remote Access Setup Link", url: "https://www.clinicalkey.com/pharmacology/login?promoCodeGroup=DID416516&promoCode=416516&associatingWith=CVS%20Co,%20General%20Access" },
         { name: "Zipline - myTasks View", url: "https://cvs.retailzipline.com/field/my_tasks" },
         { name: "Bowel Prep Billing Tips", url: "https://portaldoctool.cvs.com/retailportalfiles/radar_store/temp/fm/Proper_Billing_of_Bowel_Preps_2025_%282%29_20251126141806523.pdf" },
+        { name: "PA Technician Licensure", url: "https://portaldoctool.cvs.com/retailportalfiles/radar_store/temp/store/Pennsylvania_Incumbent_Pharmacy_Technician_Application_Checklist_2025120917021865.pdf" },
 
 
         { name: "CVS\Domain Apps Portal (Citrix) ", url: "https://remote.cvshealth.com/Citrix/remotecvsWeb/" }
